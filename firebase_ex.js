3cc4e03465e67bd05761034d1622df453d660541
KyalSinLinLett <alexsin0012@gmail.com>
2021-06-07 17:38:11 +0630

    secure firebase config

--- firebase_ex.js
+++ firebase_ex.js
@@ -3,12 +3,12 @@ import '@firebase/auth'
 import '@firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB3vM7CJAgN0dyVeYd3NxFcW-0Mv3Jmttg",
-    authDomain: "loco-chat-app.firebaseapp.com",
-    projectId: "loco-chat-app",
-    storageBucket: "loco-chat-app.appspot.com",
-    messagingSenderId: "111141300576",
-    appId: "1:111141300576:web:3c7bc55fe68a46f5a5d605"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const app = firebase.initializeApp(firebaseConfig);
